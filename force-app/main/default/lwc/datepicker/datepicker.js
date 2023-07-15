b7a4410445c2dbeb2b72e8abff6f348bf3a0d8cf
Christian Pelayo <pelayochristian.dev@gmail.com>
2023-07-15 16:37:28 +0800

    add active style when selected

--- force-app/main/default/lwc/datepicker/datepicker.js
+++ force-app/main/default/lwc/datepicker/datepicker.js
@@ -14,6 +14,8 @@ export default class Datepicker extends LightningElement {
 
     _calendar;
     _date = null;
+    _selectedDayElement = null;
+    _datepickerContainer = null;
 
     constructor() {
         super();
@@ -25,14 +27,21 @@ export default class Datepicker extends LightningElement {
     }
 
     renderedCallback() {
+        this._datepickerContainer = this.template.querySelector('[data-id="datepicker-main_container"]');
         this.renderMonthDaysElement();
     }
 
+    /**
+     * Method used to navigation previous month.
+     */
     prevMonth() {
         this._calendar.goToPreviousMonth();
         this.month = this._calendar.month.name;
     }
 
+    /**
+     * Method used to navigation next month.
+     */
     nextMonth() {
         this._calendar.goToNextMonth();
         this.month = this._calendar.month.name;
@@ -44,15 +53,6 @@ export default class Datepicker extends LightningElement {
             date.monthNumber === this._date.monthNumber &&
             date.year === this._date.year
         );
-    }
-
-    get getWeekDaysHeader() {
-        const result = this._calendar.weekDays.map((weekDay) => ({
-            fullName: `defaultPicker-${weekDay}`,
-            shortName: weekDay.substring(0, 3),
-        }));
-
-        return result;
     }
 
     getMonthDaysMatrix() {
@@ -82,37 +82,105 @@ export default class Datepicker extends LightningElement {
         return monthDaysMatrix;
     }
 
+    /**
+     * Construct the DOM Element of the Weekdays to set
+     * as a header of the calendar.
+     *
+     * @returns <Object> DOM Element
+     */
+    getWeekDaysHeader() {
+        // Creating the <tr> element
+        const trElement = document.createElement('tr');
+        trElement.setAttribute('id', 'defaultPicker-weekdays');
+
+        for (let i = 0; i < this._calendar.weekDays.length; i++) {
+            const dayStr = this._calendar.weekDays[i];
+            const thElement = document.createElement('th');
+            thElement.setAttribute('id', dayStr);
+            thElement.setAttribute('scope', 'col');
+            thElement.setAttribute('key', dayStr);
+
+            const abbrElement = document.createElement('abbr');
+            abbrElement.setAttribute('title', dayStr);
+            abbrElement.textContent = dayStr.substring(0, 3);
+
+            thElement.appendChild(abbrElement);
+            trElement.appendChild(thElement);
+        }
+
+        return trElement;
+    }
+
     renderMonthDaysElement() {
         const monthDaysMatrix = this.getMonthDaysMatrix();
-        let tbodyWeekdaysStr = '';
+        const tbodyWeekdaysElement = document.createElement('tbody');
+
+        console.log(this._date);
         for (let i = 0; i < monthDaysMatrix.length; i++) {
             const subarray = monthDaysMatrix[i];
-            tbodyWeekdaysStr += `<tr>`;
+            const trElement = document.createElement('tr');
+
             for (let j = 0; j < subarray.length; j++) {
                 const day = subarray[j];
                 const key = `${day.date} ${day.month} ${day.year}`;
 
+                const tdElement = document.createElement('td');
+                tdElement.setAttribute('aria-selected', 'false');
+                tdElement.setAttribute('role', 'gridcell');
+                tdElement.setAttribute('tabindex', '0');
+                tdElement.setAttribute('aria-label', key);
+
+                const spanElement = document.createElement('span');
+                spanElement.setAttribute('class', 'slds-day');
+                spanElement.textContent = day.date;
+                // eslint-disable-next-line no-unused-vars
+                spanElement.addEventListener('click', (e) => this.handleSelectDay(tdElement, day));
+                tdElement.appendChild(spanElement);
+
                 if (day.monthNumber === this._calendar.month.number) {
-                    tbodyWeekdaysStr += `<td aria-selected="false" class="" role="gridcell" tabindex="0" aria-label="${key}">
-                                            <span class="slds-day">${day.date}</span>
-                                        </td>`;
+                    tdElement.setAttribute('class', '');
                 } else {
-                    tbodyWeekdaysStr += ` <td aria-selected="false" class="slds-day_adjacent-month" role="gridcell" aria-label="${key}">
-                                            <span class="slds-day">${day.date}</span>
-                                        </td>`;
+                    tdElement.setAttribute('class', 'slds-day_adjacent-month');
                 }
 
-                // TODO
                 if (this.isSelectedDate(day)) {
-                    // el.classList.add('selected');
-                    // this.selectedDayElement = el;
+                    tdElement.classList.add('slds-is-selected');
+                    this._selectedDayElement = tdElement;
                 }
+
+                trElement.appendChild(tdElement);
             }
-            tbodyWeekdaysStr += `<tr>`;
+
+            tbodyWeekdaysElement.appendChild(trElement);
         }
 
-        const element = this.template.querySelector(`[data-id="datepicker-weekdays"]`);
-        // eslint-disable-next-line @lwc/lwc/no-inner-html
-        element.innerHTML = tbodyWeekdaysStr;
+        const tableElement = this.template.querySelector('[data-id="datepicker-table"]');
+        while (tableElement.firstChild) {
+            tableElement.removeChild(tableElement.firstChild);
+        }
+
+        // Append the Header and the Body to the Calendar table.
+        tableElement.appendChild(this.getWeekDaysHeader());
+        tableElement.appendChild(tbodyWeekdaysElement);
+    }
+
+    handleSelectDay(element, day) {
+        if (day.isEqualTo(this._date)) return;
+
+        this._date = day;
+
+        if (day.monthNumber !== this._calendar.month.number) {
+            this.prevMonth();
+        } else {
+            element.classList.add('slds-is-selected');
+            this._selectedDayElement.classList.remove('slds-is-selected');
+            this._selectedDayElement = element;
+        }
+
+        this._datepickerContainer.classList.remove('slds-is-open');
+    }
+
+    toggleDatepicker() {
+        this._datepickerContainer.classList.add('slds-is-open');
     }
 }
